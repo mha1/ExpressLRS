877fca373ee58e68bed3af85e274876fdf5b6f9b
Michael <101.37584@germanynet.de>
2024-03-18 12:37:41 +0100

    moved SUMD preparation to separate base class for code sharing

--- src/src/rx-serial/SerialSUMD.h
+++ src/src/rx-serial/SerialSUMD.h
@@ -1,25 +1,31 @@
+#pragma once
+
 #include "SerialIO.h"
 #include "crc.h"
 
 #define SUMDBAUD    115200
 #define SUMDCONFIG  SERIAL_8N1
 
-class SerialSUMD : public SerialIO {
+class SUMD {
 public:
-    explicit SerialSUMD(Stream &out, Stream &in) : SerialIO(&out, &in) { crc2Byte.init(16, 0x1021); }
+    SUMD() { crc2Byte.init(16, 0x1021); }
+    ~SUMD() {}
 
-    virtual ~SerialSUMD() {}
-
-    Crc2Byte crc2Byte;
-
-    void setLinkQualityStats(uint16_t lq, uint16_t rssi) override;
-    uint32_t sendRCFrameToFC(bool frameAvailable, uint32_t *channelData) override;
-    void sendMSPFrameToFC(uint8_t* data) override;
-    void sendLinkStatisticsToFC() override;
+    void prepareSUMD(uint32_t *channelData, uint8_t* outBuffer); 
 
 private:
-    uint16_t linkQuality = 0;
-    uint16_t rssiDBM = 0;
+    Crc2Byte crc2Byte;
+};
 
-    void processByte(uint8_t byte) override {};
+class SerialSUMD : SUMD, public SerialIO {
+public:
+    explicit SerialSUMD(Stream &out, Stream &in) : SUMD(), SerialIO(&out, &in) {}
+    virtual ~SerialSUMD() {}
+
+    void queueLinkStatisticsPacket() override {}
+    void queueMSPFrameTransmission(uint8_t* data) override {}
+    uint32_t sendRCFrame(bool frameAvailable, bool frameMissed, uint32_t *channelData) override;
+
+private:
+    void processBytes(uint8_t *bytes, uint16_t size) override {};
 };
