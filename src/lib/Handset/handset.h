d65982606bc2875d1dd5925c54ecadccabd2b0b5
Michael <101.37584@germanynet.de>
2024-11-05 22:12:38 +0100

    cleanup

--- src/lib/Handset/handset.h
+++ src/lib/Handset/handset.h
@@ -117,6 +117,6 @@ protected:
     int32_t RequestedRCpacketInterval = 5000; // default to 200hz as per 'normal'
 };
 
-#ifdef TARGET_TX || defined (UNIT_TEST)
+#if defined(TARGET_TX) || defined (UNIT_TEST)
 extern Handset *handset;
 #endif
