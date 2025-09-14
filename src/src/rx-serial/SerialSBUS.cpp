946bb4693145e07bbed399d4b587a5c638a6034f
Michael <101.37584@germanynet.de>
2025-09-14 16:36:56 +0200

    inital commit

--- src/src/rx-serial/SerialSBUS.cpp
+++ src/src/rx-serial/SerialSBUS.cpp
@@ -62,11 +62,11 @@ uint32_t SerialSBUS::sendRCFrame(bool frameAvailable, bool frameMissed, uint32_t
         PackedRCdataOut.ch1 = channelData[1];
         PackedRCdataOut.ch2 = channelData[2];
         PackedRCdataOut.ch3 = channelData[3];
-        PackedRCdataOut.ch4 = channelData[4];
+        PackedRCdataOut.ch4 = channelData[8];   // remapped to ETX CH9 to SBUS CH5
         PackedRCdataOut.ch5 = channelData[5];
         PackedRCdataOut.ch6 = channelData[6];
         PackedRCdataOut.ch7 = channelData[7];
-        PackedRCdataOut.ch8 = channelData[8];
+        PackedRCdataOut.ch8 = channelData[4];   // remapped to ETX CH5 to SBUS CH9
         PackedRCdataOut.ch9 = channelData[9];
         PackedRCdataOut.ch10 = channelData[10];
         PackedRCdataOut.ch11 = channelData[11];
